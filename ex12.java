918d7c071f169d0f110b402c3619e55dfbad3bca
VinyEditor <111810869+VinyEditor@users.noreply.github.com>
2022-09-13 14:33:13 -0300

    Add files via upload
    Exercicios de JAVA

---
+++ ex12.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class ex12 {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+
+		double base, altura, area;
+
+		System.out.printf("Digite o valor da base: ");
+		base = ler.nextDouble();
+
+		System.out.printf("Digite o valor da altura: ");
+		altura = ler.nextDouble();
+
+		ler.close();
+
+		area = base * altura;
+
+		if (area > 100) {
+			System.out.printf("Terreno grande");
+		}
+
+		else {
+			System.out.printf("Terreno pequeno");
+		}
+
+	}
+
+}
